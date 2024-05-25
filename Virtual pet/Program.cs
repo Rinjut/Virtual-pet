bc9fdd4dfa64874050f84d253d2fb0cab57a4a4a
Rinjut <Rthankammamathe9698@conestogac.on.ca>
2024-05-25 15:54:04 -0400

    Add project files.

---
+++ Virtual pet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace virtualPetSimulator
+{
+    class Pets
+    {
+        static void main(String[] args)
+        {
+            Console.WriteLine("Enter your pet type \n.1 Fish \n 2. Dog \n 3. parrot \n");
+            string petType = Console.ReadLine();
+            Console.WriteLine("Enter pet name");
+            string petName = Console.ReadLine();
+            Console.WriteLine("Welcome" + petName);
+        }
+    }
+}
